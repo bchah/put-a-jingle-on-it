9765d3afc3f06aecc92a0673b18e42c5024c3009
Brian Chahley <help@beamwell.com>
2021-02-16 21:44:17 -0500

    Add a bunch of jingles

--- jingle.js
+++ jingle.js
@@ -4,72 +4,202 @@ var jingles = [
         name: "Sentimental Boys",
         notes: "Thinking of the good times behind us, looking forward to even better times ahead.",
         src: "./jingles/1.mp3",
-        artist: "Karl"
+        artist: "Karl",
+        created: "2021-02-12"
     },
     {
         id: 2,
         name: "Ease-Us",
         notes: "Bringing you peacefully into another beautiful day on the California coast.",
         src: "./jingles/2.mp3",
-        artist: "Karl"
+        artist: "Karl",
+        created: "2021-02-12"
     },
     {
         id: 3,
         name: "A Great Honor",
         notes: "Creating a sense of hope, ready for the future to reward it.",
         src: "./jingles/3.mp3",
-        artist: "Karl"
+        artist: "Karl",
+        created: "2021-02-12"
     },
     {
         id: 4,
         name: "I Love You",
         notes: "An ethereal, prosperous declaration of pure love. Nothing is more powerful.",
         src: "./jingles/4.mp3",
-        artist: "Karl"
+        artist: "Karl",
+        created: "2021-02-12"
     },
     {
         id: 5,
         name: "Bubbly Hope",
         notes: "This bubbly motif makes you wonder what's coming up next, while still staying cute and playful.",
         src: "./jingles/5.mp3",
-        artist: "Karl"
+        artist: "Karl",
+        created: "2021-02-12"
     },
     {
         id: 6,
         name: "Organ Fonk",
         notes: "A catchy and upbeat organ groove, to introduce the most wholesome of projects.",
         src: "./jingles/6.mp3",
-        artist: "Karl"
+        artist: "Karl",
+        created: "2021-02-12"
     },
     {
         id: 7,
         name: "Open Sparkles",
         notes: "The future is now! This sparkly riff is sure to pique interest in your latest creation.",
         src: "./jingles/7.mp3",
-        artist: "Karl"
+        artist: "Karl",
+        created: "2021-02-12"
     },
     {
         id: 8,
         name: "Keys in the Jar",
         notes: "An exotic, mystical exploration that reaches into your soul but leaves you standing firmly on earth.",
         src: "./jingles/8.mp3",
-        artist: "Karl"
+        artist: "Karl",
+        created: "2021-02-12"
     },
     {
         id: 9,
         name: "Sunrise",
         notes: "Let the gentle piano and string melody carry you all the way to a crisp and joyful 'ting'.",
         src: "./jingles/9.mp3",
-        artist: "Karl"
+        artist: "Karl",
+        created: "2021-02-12"
     },
     {
         id: 10,
         name: "Future Runner",
         notes: "One dark morning in November 2019, Los Angeles...",
         src: "./jingles/10.mp3",
-        artist: "Brian"
+        artist: "Brian",
+        created: "2021-02-11"
+    },
+    {
+        id: 11,
+        name: "Dark Jungle",
+        notes: "When you want your viewers on the edge of their seats, look no further than this mysterious riff with an electric ending.",
+        src: "./jingles/11.mp3",
+        artist: "Karl",
+        created: "2021-02-16"
+    },
+    {
+        id: 12,
+        name: "Wooded Glen",
+        notes: "A morning song for waking up and smelling the fresh-cut grass. This is life.",
+        src: "./jingles/12.mp3",
+        artist: "Karl",
+        created: "2021-02-16"
+    },
+    {
+        id: 13,
+        name: "Happy Fog",
+        notes: "A pensive moment wrapped in a happy fog, with a sprinkle of joy on top.",
+        src: "./jingles/13.mp3",
+        artist: "Karl",
+        created: "2021-02-16",
+        bpm: 95
+    },
+    {
+        id: 14,
+        name: "Quality Assurance",
+        notes: "A logical and friendly ditty, comforting as submitting an electronic report to human resources.",
+        src: "./jingles/14.mp3",
+        artist: "Karl",
+        created: "2021-02-16",
+        bpm: 84
+    },
+    {
+        id: 15,
+        name: "Back Nine",
+        notes: "This uplifting and swirling piano melody is guaranteed to lead your viewers to a happy day. Unless they are sad people.",
+        src: "./jingles/15.mp3",
+        artist: "Karl",
+        created: "2021-02-16",
+        bpm: 137
+    },
+    {
+        id: 16,
+        name: "Country Ride",
+        notes: "After a long ride in the country carriage, you finally see the town up ahead. A fanatasy awaits!",
+        src: "./jingles/16.mp3",
+        artist: "Karl",
+        created: "2021-02-16",
+        bpm: 131
+    },
+    {
+        id: 17,
+        name: "Zen Capitalism",
+        notes: "Even computers need a lullaby. Put yours to sleep with this friendly tune, sure to be virus-free.",
+        src: "./jingles/17.mp3",
+        artist: "Karl",
+        created: "2021-02-16",
+        bpm: 66
+    },
+    {
+        id: 18,
+        name: "Distant Success",
+        notes: "You're well on the way to an A+ career, but don't forget the gaping chasm which stands between you and success.",
+        src: "./jingles/18.mp3",
+        artist: "Karl",
+        created: "2021-02-16",
+        bpm: 138
+    },
+    {
+        id: 19,
+        name: "Blip Blop",
+        notes: "Someone's got a leaky pipe! This pointy motif bounces around and evokes feelings of underwater exploration.",
+        src: "./jingles/19.mp3",
+        artist: "Karl",
+        created: "2021-02-16",
+        bpm: 144
+    },
+    {
+        id: 20,
+        name: "Today is",
+        notes: "Let the day wash over you, the good parts in the bad. In the end it's all about the various times you had.",
+        src: "./jingles/20.mp3",
+        artist: "Karl",
+        created: "2021-02-16"
+    },
+    {
+        id: 21,
+        name: "Being Ethical",
+        notes: "Sometimes you need to go off the rails and be an upside-down maverick. This cacophonous track would commplement any mal-intended deed!",
+        src: "./jingles/21.mp3",
+        artist: "Karl",
+        created: "2021-02-16",
+        bpm: 111
+    },
+    {
+        id: 22,
+        name: "Closing the Deal",
+        notes: "After that final flick of the pen, you picture yourself enjoying the great reward of a deal well done - relaxing by a beautiful sunset.",
+        src: "./jingles/22.mp3",
+        artist: "Karl",
+        created: "2021-02-16",
+        bpm: 75
+    },
+    {
+        id: 23,
+        name: "Coffee Break",
+        notes: "It's your safe, happy world. Take a moment to enjoy a sip of java and snap along with this fresh roasted jam.",
+        src: "./jingles/23.mp3",
+        artist: "Karl",
+        created: "2021-02-16"
+    },
+    {
+        id: 24,
+        name: "Irish Fields",
+        notes: "Grab your 4-leaf clover, pour yourself a green beer and get ready for a good old jig - it's Irish music!",
+        src: "./jingles/14.mp3",
+        artist: "Karl",
+        created: "2021-02-16"
     }
-
 
 
 ];
@@ -100,7 +230,7 @@ function fireUpYourPotatoCannonDoctorJones() {
 
             if (page == pages) {
                 $("#moo").removeClass("no").addClass("no");
-                setTimeout(()=>{$($("#moo").removeClass("no"))},500);
+                setTimeout(() => { $($("#moo").removeClass("no")) }, 500);
                 return false;
             } else {
                 $("#jingles").hide().html("");
@@ -119,7 +249,7 @@ function fireUpYourPotatoCannonDoctorJones() {
         $(".prevPage").on("click", () => {
             if (page == 1) {
                 $("#moo").removeClass("no").addClass("no");
-                setTimeout(()=>{$($("#moo").removeClass("no"))},500);
+                setTimeout(() => { $($("#moo").removeClass("no")) }, 500);
                 return false;
             } else {
                 $("#jingles").hide().html("");
